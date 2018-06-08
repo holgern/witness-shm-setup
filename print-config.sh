351bcd21eaaab0613616b52061e2511d3ea02409
Holger Nahrstaedt <holgernahrstaedt@gmx.de>
2018-06-08 13:17:59 +0200

    fix scipt

--- print-config.sh
+++ print-config.sh
@@ -4,12 +4,12 @@
 
 usage="${yellow}Usage: print-config${normal}"
 
-echo -e "${green)Show config. ${normal}"
+echo -e "${green}Show config. ${normal}"
 
 # running as root
 if [ "$EUID" -ne 0 ]
 then
-	echo "${red}Please run this script as root${normal}"
+	echo -e "${red}Please run this script as root${normal}"
 	exit
 fi
 # check parameter usage
@@ -23,3 +23,4 @@ echo -e "shm_backup_dir: ${cyan}${shm_backup_dir}${normal}"
 echo -e "ssh_port: ${cyan}${ssh_port}${normal}"
 echo -e "data_location: ${cyan}${data_location}${normal}"
 echo -e "shared_file_size: ${cyan}${shared_file_size}${normal}"
+
